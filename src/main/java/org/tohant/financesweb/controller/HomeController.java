7637ca00ff8e0c89ea0d0c44a3035bfbcef549c2
svire <svirepa@lanit.ru>
2024-03-03 15:50:57 +0300

    changes 2

--- src/main/java/org/tohant/financesweb/controller/HomeController.java
+++ src/main/java/org/tohant/financesweb/controller/HomeController.java
@@ -3,11 +3,9 @@ package org.tohant.financesweb.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.tohant.financesweb.api.model.PaymentDto;
 import org.tohant.financesweb.api.sheet.GoogleSheetsService;
 
 @Controller
