7df24e7194dd22664e7d686a6fd0c2ad42f7d143
svire <svirepa@lanit.ru>
2024-03-25 22:04:54 +0300

    add relations for categories and profiles

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -41,3 +41,8 @@ VALUES ('nastysha');
 
 INSERT INTO profile (user_id)
 VALUES ('alexs');
+
+INSERT INTO profile_categories (profile_id, categories_id)
+VALUES (1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
+       (2, 6), (2, 7), (2, 8), (2, 9), (2, 10),
+       (3, 11), (3, 12), (3, 13), (3, 14), (3, 15);
