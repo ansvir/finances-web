a7823f693931c65f3b0f5e8a59a170962469f03d
svire <svirepa@lanit.ru>
2024-03-30 11:58:02 +0300

    update data script 4

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,10 +1,10 @@
-TRUNCATE TABLE profile;
-TRUNCATE TABLE category;
-TRUNCATE TABLE profile_categories;
-TRUNCATE TABLE app_user;
-TRUNCATE TABLE groups;
-TRUNCATE TABLE authorities;
-TRUNCATE TABLE group_authorities;
+TRUNCATE TABLE profile_categories CASCADE;
+TRUNCATE TABLE profile CASCADE;
+TRUNCATE TABLE category CASCADE;
+TRUNCATE TABLE app_user CASCADE;
+TRUNCATE TABLE groups CASCADE;
+TRUNCATE TABLE authorities CASCADE;
+TRUNCATE TABLE group_authorities CASCADE;
 
 INSERT INTO app_user (username, password, enabled)
 VALUES ('admin', '$2a$12$MPR1fNKX8fSeuTbm7h/0EOpAAc//UiURywJJqTz/0wSBh3VVqqUsG', true),
@@ -20,35 +20,35 @@ INSERT INTO authorities VALUES ('alexs', 'USER');
 INSERT INTO group_authorities VALUES (1, 'USER');
 
 -- Insert categories
-INSERT INTO category (name, priority)
-VALUES ('Долг', 1),
-       ('Другое', 2),
-       ('Квартплата', 3),
-       ('Развлечения', 4),
-       ('Связь', 5);
+INSERT INTO category
+VALUES (1, 'Долг', 1),
+       (2, 'Другое', 2),
+       (3, 'Квартплата', 3),
+       (4, 'Развлечения', 4),
+       (5, 'Связь', 5);
 
-INSERT INTO category (name, priority)
-VALUES ('Долг', 1),
-       ('Другое', 2),
-       ('Квартплата', 3),
-       ('Развлечения', 4),
-       ('Связь', 5);
+INSERT INTO category
+VALUES (6, 'Долг', 1),
+       (7, 'Другое', 2),
+       (8, 'Квартплата', 3),
+       (9, 'Развлечения', 4),
+       (10, 'Связь', 5);
 
-INSERT INTO category (name, priority)
-VALUES ('Долг', 1),
-       ('Другое', 2),
-       ('Квартплата', 3),
-       ('Развлечения', 4),
-       ('Связь', 5);
+INSERT INTO category
+VALUES (11, 'Долг', 1),
+       (12, 'Другое', 2),
+       (13, 'Квартплата', 3),
+       (14, 'Развлечения', 4),
+       (15, 'Связь', 5);
 
-INSERT INTO profile (user_id)
-VALUES ('admin');
+INSERT INTO profile
+VALUES (1, 'admin');
 
-INSERT INTO profile (user_id)
-VALUES ('nastysha');
+INSERT INTO profile
+VALUES (2, 'nastysha');
 
-INSERT INTO profile (user_id)
-VALUES ('alexs');
+INSERT INTO profile
+VALUES (3, 'alexs');
 
 INSERT INTO profile_categories (profile_id, categories_id)
 VALUES (1, 1), (1, 2), (1, 3), (1, 4), (1, 5),
